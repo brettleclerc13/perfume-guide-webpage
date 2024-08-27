98f28f5f8d320b58ed37416cf5901f4d0f6de704
brettleclerc13 <brettleclerc13@gmail.com>
2024-08-27 16:27:42 +0200

    <build> client prefers static play buttons on cards

--- discover-ado-perfumes/src/components/Cards/Cards.js
+++ discover-ado-perfumes/src/components/Cards/Cards.js
@@ -1,42 +1,16 @@
-import { useEffect, useRef, useState } from 'react';	
+import { useRef } from 'react';	
 import './Cards.css';
 
 const Cards = (img_data) => {
 	const { name, src, alt, onClick } = img_data;
-	const [isVisible, setIsVisible] = useState(false);
 	const cardRef = useRef(null);
 
-	useEffect(() => {
-		const currentCard = cardRef.current;
-		const observer = new IntersectionObserver(
-			(entries) => {
-				entries.forEach((entry) => {
-					if(entry.isIntersecting) {
-						setIsVisible(true);
-						setTimeout(() => setIsVisible(false), 1500);
-					}
-				});
-			},
-			{
-				threshold: 0.3,
-			}
-		);
-		if (currentCard) {
-			observer.observe(currentCard);
-		}
-		return () => {
-			if (currentCard) {
-				observer.unobserve(currentCard);
-			}
-		};
-	}, []);
-
 	return (
-			<div
-				className={`card ${isVisible ? 'show-play-icon' : ''}`}
-				ref={cardRef}
-				onClick={onClick}
-			>
+		<div
+			className="card"
+			ref={cardRef}
+			onClick={onClick}
+		>
 			<img
 				name={name}
 				className="card-img"
@@ -52,18 +26,14 @@ const Cards = (img_data) => {
 					{name.replace('_', ' ')}
 				</h3>
 			</span>
-			{isVisible && (
-				<div className="play-icon">
-					<svg
-						width="34"
-						height="34"
-						viewBox="0 0 24 24"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path d="M8 5v14l11-7L8 5z" fill="white"/>
-					</svg>
-				</div>
-			)}
+			<div className="play-icon">
+				<img
+					src="./Elements/Layout/play_button_v3.svg"
+					alt="Play Icon"
+					width="25"
+					height="25"
+				/>
+			</div>
 		</div>
 	);
 };
